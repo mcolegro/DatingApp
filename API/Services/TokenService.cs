5506fb38e2cb27ce4589d4ebed039db080f9afdc
mcolegro <matt@colegrove.us>
2021-03-26 20:56:04 -0400

    End of Section 13

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -23,7 +23,8 @@ namespace API.Services
          // Adding some claims
          var claims = new List<Claim>
          {
-            new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
          };
 
          // Creating the credentials
